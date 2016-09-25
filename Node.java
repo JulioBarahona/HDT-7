2f4886e58aa01b64f580cb29927b2cfbe1fa8242
CoffeeBrewer <cas15151@uvg.edu.gt>
2016-09-25 17:41:53 -0600

    Implementacion de BST

---
+++ Node.java
@@ -0,0 +1,11 @@
+/*Codigo tomado de http://algorithms.tutorialhorizon.com/binary-search-tree-complete-implementation/*/
+public class Node {
+    int data;
+    Node left;
+    Node right;
+    public Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
